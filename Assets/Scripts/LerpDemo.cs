c29c4c75334d3adc2fac512473225188f670532e
Spencer Walter <waltespe@shernet.sheridancollege.ca>
2025-01-15 10:57:42 -0500

    Added animation curves and LERP

---
+++ Assets/Scripts/LerpDemo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class LerpDemo : MonoBehaviour
+{
+    [Range(0f, 1f)] public float t;
+
+    public Transform start;
+    public Transform end;
+    public AnimationCurve curve;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector2 pos = new Vector2(Mathf.Lerp(-4, 5, t), 0);
+        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+    }
+}
